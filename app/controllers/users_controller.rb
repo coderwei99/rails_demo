1676194f14675f66264b1335f1abfac420908724
jay <916606569@qq.com>
2021-08-02 11:13:43 +0800

    finsh user signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,25 @@
 class UsersController < ApplicationController
   def new
-    
+    @user = User.new
   end
+
+  def show
+    @user = User.find(params[:id])
+  end
+
+  def create
+    @user = User.new(user_detail)
+    if @user.save
+      flash[:success] = 'let go'
+      redirect_to @user
+    else
+      render :new
+    end
+  end
+
+  private
+
+    def user_detail
+      params.require(:user).permit(:name,:email,:password,:password_confirmation)
+    end
 end
