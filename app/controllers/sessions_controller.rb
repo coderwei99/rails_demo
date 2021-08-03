89d7b5b4125ae22f0659fee5f6afd8f6b20e99c3
jay <916606569@qq.com>
2021-08-03 17:58:37 +0800

    Finish user edit, update, index, and destroy actions

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -8,7 +8,7 @@ class SessionsController < ApplicationController
     if user&&user.authenticate(params[:session][:password])
       log_in user
       params[:session][:remember_my]==='1'? remember(user) : forget(user)
-      redirect_to user
+      redirect_back_or user
     else
       flash.now[:danger] = '密码错误'
       render 'new'
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,11 @@
 class UsersController < ApplicationController
+  before_action :logged_in_user,only:[:index,:edit,:update,:destroy]
+  before_action :correct_user,only:[:edit,:update]
+
+  def index
+    @users = User.paginate(page: params[:page])
+  end
+
   def new
     @user = User.new
   end
@@ -18,8 +25,37 @@ class UsersController < ApplicationController
     end
   end
 
+  def edit
+  end
+
+  def update
+    if @user.update(user_detail)
+      flash[:success] = '修改成功'
+      redirect_to @user
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    User.find(params[:id]).destroy
+    flash[:success] = ' 删除成功'
+    redirect_to users.url
+  end
+
   private
     def user_detail
       params.require(:user).permit(:name,:email,:password,:password_confirmation)
+    end
+    def logged_in_user
+      unless loggin?
+        store_location
+        flash[:danger] = '请登录'
+        redirect_to login_url
+      end
+    end
+    def correct_user
+      @user = User.find(params[:id])
+      redirect_to(root_url) unless current_user?(@user)
     end
 end
